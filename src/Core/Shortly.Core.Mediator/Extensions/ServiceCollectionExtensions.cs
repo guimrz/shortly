f46c46449b8f6d0887f897b2a73d749773bcbd46
José <joseguimaraes96@hotmail.com>
2025-04-04 15:44:27 +0100

    Core | Mediator - Created extensions to register the handlers

--- src/Core/Shortly.Core.Mediator/Extensions/ServiceCollectionExtensions.cs
+++ src/Core/Shortly.Core.Mediator/Extensions/ServiceCollectionExtensions.cs
@@ -14,5 +14,38 @@ namespace Shortly.Core.Mediator.Extensions
 
             return services;
         }
+
+        public static IServiceCollection AddRequestHandler<THandler, TRequest, TResponse>(this IServiceCollection services)
+            where THandler : class, IRequestHandler<TRequest, TResponse>
+            where TRequest : IRequest<TResponse>
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.AddScoped<IRequestHandler<TRequest, TResponse>, THandler>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddRequestHandler<THandler, TRequest>(this IServiceCollection services)
+            where THandler : class, IRequestHandler<TRequest>
+            where TRequest : IRequest
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.AddScoped<IRequestHandler<TRequest>, THandler>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddNotificationHandler<THandler, TNotification>(this IServiceCollection services)
+            where THandler : class, INotificationHandler<TNotification>
+            where TNotification : INotification
+        {
+            ArgumentNullException.ThrowIfNull(services);
+
+            services.AddScoped<INotificationHandler<TNotification>, THandler>();
+
+            return services;
+        }
     }
 }
